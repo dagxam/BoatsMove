e103eea61cf9b72330be34b041f42073e68d5740
dagxam <dagxam@gmail.com>
2026-09-04 20:53:33 +0300

    Add ship buoyancy attitude runtime state

--- src/main/java/ru/dagxam/boatsmove/ship/ShipRuntimeState.java
+++ src/main/java/ru/dagxam/boatsmove/ship/ShipRuntimeState.java
@@ -7,32 +7,21 @@ public final class ShipRuntimeState {
     private Location position;
     private double speed;
     private double verticalSpeed;
+    private float pitch;
+    private float roll;
 
     public ShipRuntimeState(Location position) {
         this.position = position.clone();
     }
 
-    public Location position() {
-        return position.clone();
-    }
-
-    public void position(Location position) {
-        this.position = position.clone();
-    }
-
-    public double speed() {
-        return speed;
-    }
-
-    public void speed(double speed) {
-        this.speed = speed;
-    }
-
-    public double verticalSpeed() {
-        return verticalSpeed;
-    }
-
-    public void verticalSpeed(double verticalSpeed) {
-        this.verticalSpeed = verticalSpeed;
-    }
+    public Location position() { return position.clone(); }
+    public void position(Location position) { this.position = position.clone(); }
+    public double speed() { return speed; }
+    public void speed(double speed) { this.speed = speed; }
+    public double verticalSpeed() { return verticalSpeed; }
+    public void verticalSpeed(double verticalSpeed) { this.verticalSpeed = verticalSpeed; }
+    public float pitch() { return pitch; }
+    public void pitch(float pitch) { this.pitch = pitch; }
+    public float roll() { return roll; }
+    public void roll(float roll) { this.roll = roll; }
 }
